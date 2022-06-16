7f450f61af20f2d4fdae1d5062225f14098226b4
jonassteinberg1 <jsteinberg@breadfinance.com>
2022-06-16 12:00:16 -0500

    dockerized test passing in dev

--- tests/integration/dev_test.py
+++ tests/integration/dev_test.py
@@ -3,5 +3,5 @@ import os
 import requests
 
 def test_flask_root_returns_200():
-     response = requests.get(f"http://{'FLASK_URL'}:{'FLASK_PORT'}")
+     response = requests.get(f"http://{os.getenv('FLASK_URL')}:{os.getenv('FLASK_PORT')}")
      assert response.status_code == 200
