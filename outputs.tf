cc1cad852f12ce7691960161d66ad7674664baab
jonassteinberg1 <jsteinberg@breadfinance.com>
2022-06-20 12:39:07 -0500

    BRGRWRLD-4 output policy for further testing

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "burgerworld-hello-ecs-dec-ecr-repository-name" {
   value = aws_ecr_repository.burgerworld-hello-ecs-ecr.repository_url
 }
+
+output "burgerworld-hello-ecs-dec-ecr-repository-policy-document-json" {
+  value = data.aws_iam_policy_document.burgerworld-hello-ecs-ecr-permissions-policy-document.json
+}
