636b9b9bb8fc9ef06c420993424f2467668e152f
jonassteinberg1 <jsteinberg@breadfinance.com>
2022-06-23 14:31:47 -0500

    BRGRWRLD-4 ebs to 30GB

--- main.tf
+++ main.tf
@@ -265,7 +265,7 @@ resource "aws_launch_configuration" "burgerworld-hello-ecs-launch-configuration"
 
   root_block_device {
     volume_type           = "standard"
-    volume_size           = "20"
+    volume_size           = "30"
     delete_on_termination = "true"
     encrypted             = "true"
   }
